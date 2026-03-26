b565b9b2d05da2dbf5756cb0165729bbc62f47e1
unknown <gnidwqiv@naver.com>
2026-03-26 21:19:52 +0900

    2026.03.26

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("hello world")
-
-print("asdf")
+a_class = {'김철수', '이영희', '박민수', '최지영', '홍길동'}
+b_class = {'박민수', '최지영', '정수민', '강호동', '이영희'}
+print(a_class & b_class)
+print(a_class | b_class)
+print(a_class - b_class)
+print(b_class - a_class)
