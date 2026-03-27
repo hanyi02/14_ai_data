2c06524293f7f30ee0ac7b2874406ed37c84e85a
unknown <gnidwqiv@naver.com>
2026-03-27 21:15:46 +0900

    2026.03.27

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-a_class = {'김철수', '이영희', '박민수', '최지영', '홍길동'}
-b_class = {'박민수', '최지영', '정수민', '강호동', '이영희'}
-print(a_class & b_class)
-print(a_class | b_class)
-print(a_class - b_class)
-print(b_class - a_class)
+height=float(input("키를 입력하세요(cm): "))
+weight=float(input("몸무게를 입력하세요(kg): "))
+height_m= height/100
+bmi= weight/(height_m **2)
+
+if bmi< 18.5:
+    print("저체중")
+elif bmi<23:
+    print("정상")
+elif bmi<25:
+    print("과체중")
+else:
+    print("비만")
+
