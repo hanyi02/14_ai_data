9e31bf2ce4ed5a0e89106ffae6d7617846632cea
unknown <gnidwqiv@naver.com>
2026-04-02 21:12:12 +0900

    2026.04.02

--- 05_Flask/app.py
+++ 05_Flask/app.py
@@ -1,18 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
-@app.route('/hello')
+
+@app.route("/hello")
 def hello():
-    return '만나서 반갑습니다'
+    return "만나서 반갑습니다."
 
 @app.route("/user/<userId>")
 def profile(userId):
     return f"{userId}\' profile"
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
